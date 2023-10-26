f04e3888607768ac20b6c592e64fb39e9791df72
jarilton <jarilton.pereira@grupofocus.com.br>
2023-10-26 11:57:00 -0300

    feat: wrapper teste

--- src/contexts/CityContext.test.tsx
+++ src/contexts/CityContext.test.tsx
@@ -1,9 +1,12 @@
 import { useCity } from "@hooks/useCity";
 import { renderHook } from "@testing-library/react-native";
+import { CityProvider } from "./CityContext";
 
 describe("Context: CityContext", () => {
   it("should be change selected city", () => {
-    const { result } = renderHook(() => useCity());
+    const { result } = renderHook(() => useCity(), {
+      wrapper: CityProvider,
+    });
 
     console.log(result);
   });
